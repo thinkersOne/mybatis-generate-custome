926d6e48d13651913f2b9f3f062cffd8ef681ff5
2415612109@qq.com <2415612109@qq.com>
2020-09-09 22:28:41 +0800

    1

---
+++ src/main/java/com/baomidou/daoExt/LogDaoExt.java
@@ -0,0 +1,16 @@
+package com.baomidou.daoExt;
+
+import com.baomidou.entity.Log;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户操作日志 DaoExt 接口
+ * </p>
+ *
+ * @author lizhihao
+ * @since 2020-09-09
+ */
+public interface LogDaoExt extends BaseMapper<LogEntity> {
+
+}
